627656713889820511d170e707e0223df5b39e64
AtomicPotatoCoder <31140021+AtomicPotatoCoder@users.noreply.github.com>
2017-08-19 16:03:23 +0200

    Farve ændring

--- 2WDS.py
+++ 2WDS.py
@@ -4,7 +4,7 @@ import pygame
 
 pygame.init()
 
-red = (255, 0, 0)
+violet = (255, 0, 150)
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("2W Druk Spil")
@@ -17,7 +17,7 @@ while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
-    gameDisplay.fill(red)
+    gameDisplay.fill(violet)
     pygame.display.update()
 
 pygame.quit()
