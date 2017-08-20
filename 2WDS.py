d69fe67ffc86693ef15de7d8bb3263d372129259
AtomicPotatoCoder <31140021+AtomicPotatoCoder@users.noreply.github.com>
2017-08-20 14:50:03 +0200

    K

--- 2WDS.py
+++ 2WDS.py
@@ -12,7 +12,7 @@ class Game():
 
 	gameDisplay = pygame.display.set_mode((800,600))
 	title_font = pygame.font.SysFont("Merryweather", 50)
-	button = pygame.Rect(100, 100, 50, 50)
+	button = pygame.Rect(310, 270, 200, 50)
 	button_font = pygame.font.SysFont("Times New Roman", 25)
 
 	def __init__(self):
@@ -24,8 +24,8 @@ class Game():
 		self.start_screen()
 
 	def start_screen(self):
-		pygame.draw.rect(self.gameDisplay, [255, 0, 0], self.button)
-		self.StartButton = self.button_font.render("Start Spillet", 1, (0,0,0))
+		pygame.draw.rect(self.gameDisplay, [250, 250, 250], self.button)
+		self.StartButton_text = self.button_font.render("Start Spillet", 1, (0,0,0))
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -35,15 +35,8 @@ class Game():
 					if self.button.collidepoint(event.pos):
 						print("collide")
 
-    		#Button interaction color
-			mouse = pygame.mouse.get_pos()
-			if 310+200 > mouse[0] > 310 and 270+50 > mouse[1] > 270:
-				pygame.draw.rect(self.gameDisplay, self.light_gray, [310, 270, 200, 50])
-			else:
-				pygame.draw.rect(self.gameDisplay, self.white, [310, 270, 200, 50])
-
     		#Start button text
-			self.gameDisplay.blit(self.StartButton, (350, 280))
+			self.gameDisplay.blit(self.StartButton_text, (350, 280))
 
 			pygame.display.update()
 
