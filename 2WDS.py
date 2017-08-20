c40a1a27db3addcee81ebbf4676b49f532d01685
AtomicPotatoCoder <31140021+AtomicPotatoCoder@users.noreply.github.com>
2017-08-20 09:39:22 +0200

    Start button interaction color

--- 2WDS.py
+++ 2WDS.py
@@ -8,6 +8,7 @@ pygame.font.init()
 violet = (255, 0, 150)
 black = (0, 0, 0)
 white = (250, 250, 250)
+light_gray = (190, 190, 190)
 
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("2W Druk Spil")
@@ -23,8 +24,13 @@ while not gameExit:
     Title_text = pygame.font.SysFont("Merryweather", 50)
     Title = Title_text.render("2W Druk Spil", 1, (0,0,0))
     gameDisplay.blit(Title, (300,200))
-    #Start button layout
-    pygame.draw.rect(gameDisplay, white, [310, 270, 200, 50])
+    #Button interaction color
+    mouse = pygame.mouse.get_pos()
+    
+    if 310+200 > mouse[0] > 310 and 270+50 > mouse[1] > 270:
+        pygame.draw.rect(gameDisplay, light_gray, [310, 270, 200, 50])
+    else:
+        pygame.draw.rect(gameDisplay, white, [310, 270, 200, 50])
     #Start button text
     start_button = pygame.font.SysFont("Times New Roman", 25)
     StartButton = start_button.render("Start Spillet", 1, (0,0,0))
