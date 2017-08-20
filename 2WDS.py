d79cde21923e944b2a9d3a7ea2b496d3bbfd98d8
Daniel Valentin <saktomail@gmail.com>
2017-08-20 15:03:35 +0200

    Refactor

--- 2WDS.py
+++ 2WDS.py
@@ -15,17 +15,32 @@ class Game():
 	button = pygame.Rect(310, 270, 200, 50)
 	button_font = pygame.font.SysFont("Times New Roman", 25)
 
+	screen = "start"
+
 	def __init__(self):
 		pygame.display.set_caption("2W Druk Spil")
 		self.gameDisplay.fill(self.violet)
 		pygame.display.set_caption("2W Druk Spil")
 		Title = self.title_font.render("2W Druk Spil", 1, (0,0,0))
 		self.gameDisplay.blit(Title, (300,200))
-		self.start_screen()
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+			if self.screen == "start":
+				self.start_screen()
+			if self.screen == "test":
+				self.test_screen()
+	
+	def test_screen(self):
+		print("TEST")
+		pygame.display.update()
 
 	def start_screen(self):
+		print("START")
 		pygame.draw.rect(self.gameDisplay, [250, 250, 250], self.button)
-		self.StartButton_text = self.button_font.render("Start Spillet", 1, (0,0,0))
+		self.start_button_text = self.button_font.render("Start Spillet", 1, (0,0,0))
 		while True:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -33,11 +48,9 @@ class Game():
 					sys.exit()
 				if event.type == pygame.MOUSEBUTTONUP:
 					if self.button.collidepoint(event.pos):
-						print("collide")
-
-    		#Start button text
-			self.gameDisplay.blit(self.StartButton_text, (350, 280))
-
+						self.screen = "test"
+						return
+			self.gameDisplay.blit(self.start_button_text, (350, 280))
 			pygame.display.update()
 
 if __name__ == '__main__':
